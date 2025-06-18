4e0c217479be1dd87a0de7d81bcadcf38a8f3fe3
pdoubleu30 <phwonka@gmail.com>
2025-06-18 17:02:54 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
 const form = document.getElementById('form');
 const eAddress = document.getElementById('email');
-const eAddressError = document.getElementById('inputError');
 
 const handleSubmit = (e) => {
   e.preventDefault();
 
      if (eAddress.value.length === 0){
+    eAddress.classList.add("custom-email");
     document.getElementById('emailError').style.display = "block";
-    document.getElementById('inputError').style.display = "block";
-    document.getElementById('email').style.display = "none";
     return;
     } else {
     document.getElementById('success').style.display = "block";
