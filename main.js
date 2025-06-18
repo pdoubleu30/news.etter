27b68bced5a484db271eb95f369f9a68c911c27f
pdoubleu30 <phwonka@gmail.com>
2025-06-18 06:26:20 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
 const form = document.getElementById('form');
 const eAddress = document.getElementById('email');
+const eAddressError = document.getElementById('inputError');
 
 const handleSubmit = (e) => {
   e.preventDefault();
+
+     if (eAddress.value.length === 0){
+    document.getElementById('emailError').style.display = "block";
+    document.getElementById('inputError').style.display = "block";
+    document.getElementById('email').style.display = "none";
+    return;
+    } else {
+    document.getElementById('success').style.display = "block";
+    document.getElementById('mainPage').style.display = "none";
+    }
 
 const formData = new FormData(e.target);
 const data = Object.fromEntries(formData);
@@ -10,12 +21,6 @@ const data = Object.fromEntries(formData);
 console.log(data);
 
 };
-
-
-const isValidEmail = () => {
-  if (email.value.length === 0);
-    return 'Please input an email.'
-}
 
 
 form.addEventListener('submit', handleSubmit);
